2c1d9822a741fa47a90031582b995d5182ca566e
gradyroc <13802504607@163.com>
2020-04-01 22:41:14 +0800

    使用枚举错误码统一处理异常

---
+++ exceptionSys/src/main/java/cn/grady/exceptions/RequestException.java
@@ -0,0 +1,10 @@
+package cn.grady.exceptions;
+
+/**
+ * @author rociss
+ * @version 1.0, on 22:31 2020/4/1.
+ */
+public class RequestException extends RuntimeException{
+
+    private static final long serialVersionUID = 8093596867786502598L;
+}
